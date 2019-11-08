562e7e1f916995189b0d64c2bd0f5ad2787fde40
abissell <code@getbraintree.com>
2019-11-08 17:58:54 +0000

    complete chapter 3 exercises

--- ch-3/ticket.rb
+++ ch-3/ticket.rb
@@ -1,8 +1,20 @@
 class Ticket
-  def initialize(venue, date)
+  def initialize(venue)
     @venue = venue
-    @date = date
     @price = 100.00
+  end
+  def date=(date)
+    year, month, day = date.split("-")
+    err = "Please submit the date in the format 'yyyy-mm-dd'."
+    if year.to_i < 1000 or year.to_i > 9999
+      puts err
+    elsif month.to_i < 1 or month.to_i > 12
+      puts err
+    elsif day.to_i > 31 or day.to_i < 1
+      puts err
+    else
+      @date = date
+    end
   end
   def venue
     @venue
@@ -18,9 +30,11 @@ class Ticket
   end
 end
 
-th = Ticket.new("Town Hall", "2013-11-12")
-cc = Ticket.new("Convention Center", "2014-12-13")
+th = Ticket.new("Town Hall")
+th.date = "2013-11-12"
+cc = Ticket.new("Convention Center")
+cc.date = "2014-12-13"
 puts "We've created two tickets."
 puts "The first is for a #{th.venue} event on #{th.date}"
 puts "The second is for an event on #{cc.date} at #{cc.venue}"
-puts "The ticket for #{th.venue} has been discounted 15% to #{th.discount(15)}"
+puts "The ticket for #{th.venue} has been discounted 15% to #{"%.2f" % th.discount(15)}"
